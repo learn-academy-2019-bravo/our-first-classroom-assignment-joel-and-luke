a6f5d87b2edd4960cbdd4ad739c30e6b9fe00a68
learn <learn@learn15.local>
2019-04-16 16:54:49 -0700

    added js-day-2.js

---
+++ js-day-2.js
@@ -0,0 +1,105 @@
+// var itemPrice = 50
+// if (itemPrice <= 100)
+//     {console.log("in budget")
+// }
+
+// var hungry = false
+// if (hungry === true) {
+//     console.log('eat food')
+// } else {
+//     console.log('keep coding')
+// }
+
+// var trafficLight = "kasdgkadskjg"
+// if (trafficLight === "green") {console.log("go")
+// } else if (trafficLight ==="yellow") {console.log("slow down");
+// } else if (trafficLight === "red"){console.log("stop");
+// } else {console.log("error")}
+
+// var numFirst = 25
+// var numSecond = 25
+//
+// if (numFirst > numSecond) {
+//     console.log(numFirst)
+// } else if (numFirst < numSecond) {
+//     console.log(numSecond)
+// } else if (numFirst === numSecond) {
+//     console.log('equal')
+// } else {
+//     console.log('error');
+// }
+
+// var aNumber = 'jkhasdghkj'
+
+
+// if (aNumber === 0) {
+//     console.log('Its zero!')
+// } else if (aNumber % 2 === 0) {
+//     console.log('its even!');
+// } else if (typeof aNumber != 'number') {
+//     console.log("error")
+// } else {
+//     console.log("its odd!")
+// }
+
+// var gradePercent = 50
+// if (gradePercent === 100) {console.log("perfect score");
+// } else if (90 <= gradePercent && gradePercent < 100)
+//     {console.log("A")
+// } else if (80 <= gradePercent && gradePercent < 90) {console.log("B")
+// } else if (70 <= gradePercent && gradePercent < 80) {console.log("C")
+// } else if (60 <= gradePercent && gradePercent < 70) {console.log("D")
+// } else if (0 < gradePercent && gradePercent < 60) {console.log("F")
+// } else if (gradePercent === 0) {console.log("no grade available")
+// } else if (typeof gradePercent != 'number') {console.log('error');
+// } else {console.log('Good work');
+// }
+
+// var dataType = true
+// if (typeof dataType === "string") {console.log("string")
+// } else if (typeof dataType === "number") {console.log("number");
+// } else if (typeof dataType === "boolean") {console.log("boolean")
+// }
+// var groceryList2 = ["chips", "dip", "cookies"]
+// groceryList2.push("soda")
+// console.log(groceryList2)
+
+// var numbers2 = [2, 4, 6, 8, 10]
+// numbers2.unshift(0)
+// console.log(numbers2);
+
+// var groceryList1 = ["apples", "carrots", "oatmeal"]
+// console.log(groceryList1.concat("granola"))
+
+// var numbers1 = [1, 2, 3, 4, 5]
+//
+// console.log("0, ".concat(numbers1))
+
+// var numbers3 = [2, 13, 6, 8, 2]
+// console.log(numbers3.indexOf(2))
+// console.log(numbers3.lastIndexOf(2))
+
+// var chars = ["y", "a", "r", "r", "a"]
+// var newchars = chars.join("")
+// console.log(newchars);
+// var reversechars = chars.reverse()
+// console.log(reversechars)
+// var newreversechars = reversechars.join("-")
+// console.log(newreversechars);
+// var reversechars2 = reversechars.join("")
+// console.log(reversechars2);
+
+// var siblings = ["Jason", "Joey"]
+// var parents = ["Jose", "Estrella"]
+// var family = siblings.concat(parents)
+// console.log(family.sort());
+// console.log(family.reverse())
+
+// var userQuestion = 'Will I get a good job after LEARN?'
+// var magicBall = ['Yup!', 'Fuhgeddaboudit', 'Maybe', 'Ask: what would your mother do?', 'Ask: what would an Australian do?', 'then do the opposite', 'No']
+// var magicBall_Length = magicBall.length
+// var randomIndex = Math.floor(Math.random()*magicBall_Length)
+// console.log(userQuestion);
+// console.log(magicBall[randomIndex]);
+//
+// object.onclick = function(){myScript};
